7524656b01322404fcd0f57c80903d8cc146164c
Alexey Lapshin <alexey.lapshin@espressif.com>
2021-11-30 19:42:48 +0300

    gdb: add simesp board for testing

---
+++ gdb/testsuite/esp_bin_generator/src/entry.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <setjmp.h>
+
+extern char _end;
+extern char __bss_start;
+
+//extern int main(int argc, const char** argv);
+extern void syscalls_init(void);
+extern void ets_delay_us(uint32_t us);
+
+void entry()
+{
+    /* give us a bit of time to connect with miniterm after esptool.py load_ram is done */
+    ets_delay_us(4000000);
+
+    /* try to re-initialize everything */
+    memset(&__bss_start, 0, &_end - &__bss_start);
+    syscalls_init();
+
+    main();
+}
+
